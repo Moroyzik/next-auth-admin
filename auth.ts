27fe7c58b39ddda94b10571f956b66fd9a5e6d89
d.morozov <d.morozov@artjoker.team>
2024-05-13 17:20:16 +0300

    migrate db and add type for role

--- auth.ts
+++ auth.ts
@@ -1,16 +1,46 @@
-import NextAuth from "next-auth";
-import {PrismaAdapter} from '@auth/prisma-adapter';
+import NextAuth, { DefaultSession } from "next-auth";
+import { PrismaAdapter } from "@auth/prisma-adapter";
+import { UserRole } from "@prisma/client";
 
 import authConfig from "@/auth.config";
-import {db} from '@/lib/db'
+import { db } from "@/lib/db";
+import { getUserById } from "@/data/user";
 
 export const {
   handlers: { GET, POST },
   auth,
   signIn,
-  signOut
+  signOut,
 } = NextAuth({
+  callbacks: {
+    async session({ token, session, user }) {
+      if (session.user && token.sub) {
+        session.user.id = token.sub;
+      }
+
+      if (token.role && session.user) {
+        session.user.role = token.role as UserRole;
+      }
+
+      return session;
+    },
+    async jwt({ token }) {
+      if (!token.sub) {
+        return token;
+      }
+
+      const existingUser = await getUserById(token.sub);
+
+      if (!existingUser) {
+        return token;
+      }
+
+      token.role = existingUser.role;
+
+      return token;
+    },
+  },
   adapter: PrismaAdapter(db),
-  session: {  strategy: 'jwt' },
+  session: { strategy: "jwt" },
   ...authConfig,
 });
